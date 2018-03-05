e0380caa46b46db9d4d59a5265b119783ae51e0d
Denis Vabishchevich <slad_den@mail.ru>
2018-03-05 23:08:41 +0300

    3.Work with return screen

--- app/src/main/java/android/bignerdranch/com/chronometr1/MainActivity.java
+++ app/src/main/java/android/bignerdranch/com/chronometr1/MainActivity.java
@@ -5,25 +5,40 @@ import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Chronometer;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
     Chronometer mChronomer;
     long timeWhenStop;
+    long timeBefReturn;
     boolean isStarted;
+
+    private static  final String KEY_INDEX="index";
+    private static  final String KEY_INDEX_IS_START="index_is_start";
+    private static  final String KEY_INDEX_BEF_RET="index_bef_ret";
+    private static  final String TAG="MainActivity";
 //Тестовый комментарий
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         mChronomer = findViewById(R.id.chronomer2);
+
+//        if(savedInstanceState!=null){
+//            timeWhenStop = savedInstanceState.getLong(KEY_INDEX);
+//        }
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,17 +63,51 @@ public class MainActivity extends AppCompatActivity {
             mChronomer.setBase(SystemClock.elapsedRealtime());
             isStarted=false;
             timeWhenStop = 0;
+            fab.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_media_play));
             return true;
         }
         });
     }
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState){
+        super.onRestoreInstanceState(saveInstanceState);
+        //mChronomer.setBase(mChronomer.getBase()- SystemClock.elapsedRealtime());
 
+        timeWhenStop=saveInstanceState.getLong(KEY_INDEX);
+        isStarted = saveInstanceState.getBoolean(KEY_INDEX_IS_START);
+        timeBefReturn = saveInstanceState.getLong(KEY_INDEX_BEF_RET);
+
+    //    Toast.makeText(this,String.valueOf(timeBefReturn),Toast.LENGTH_LONG).show();
+        if (!isStarted){
+            mChronomer.stop();
+            mChronomer.setBase(SystemClock.elapsedRealtime()+timeWhenStop);
+         }else {
+            mChronomer.start();
+            mChronomer.setBase(timeBefReturn);
+        }
+
+
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+      //  timeWhenReturn=timeWhenStop;
+        Log.i(TAG,"onSaveInstanceState");
+        savedInstanceState.putLong(KEY_INDEX,timeWhenStop);
+
+        timeBefReturn = mChronomer.getBase();
+        savedInstanceState.putLong(KEY_INDEX_BEF_RET,timeBefReturn);
+        savedInstanceState.putBoolean(KEY_INDEX_IS_START,isStarted);
+    }
+
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -74,4 +123,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    */
+
 }
